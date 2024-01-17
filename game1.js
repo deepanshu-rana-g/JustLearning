c04e404eda47d01b37b960dea07826d2ee73c66d
deepanshu-rana-g <ranajis1978@gmail.com>
2024-01-17 23:53:55 +0530

    first commit

---
+++ game1.js
@@ -0,0 +1,83 @@
+
+console.log("jai shree ram!");
+
+
+
+let userClickedPattern =[];
+console.log(userClickedPattern);
+
+let gamePattern = [];
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+let randomChosenColour = buttonColours[nextSequence()];
+console.log(randomChosenColour);
+
+gamePattern.push(randomChosenColour);
+// console.log(gamePattern);
+
+function nextSequence() {
+    let n = Math.random();
+    n = n * 4;
+    let randomNumber = Math.floor(n);
+    return randomNumber;
+    console.log(randomNumber);
+}
+function handler(event) {
+    let userChosenColour  = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    sound(userChosenColour);
+    console.log(userClickedPattern);
+    // console.log(userChosenColour);
+}
+
+
+
+$(".btn").click(handler);
+
+
+function animations() {
+    $("#" + randomChosenColour).addClass("pressed");
+    setTimeout(() => {
+        $("#" + randomChosenColour).removeClass("pressed");
+    }, 100);
+    console.log("animation played")
+}
+function sound(name) {
+    let sound = new Audio('sounds/'+ name +'.mp3');
+    sound.play();
+}
+function soundAndAnimation(randomChosenColour) {
+    switch (randomChosenColour) {
+        case "green":
+            animations();
+            // let green = new Audio('sounds/green.mp3');
+            // green.play();
+            sound(green);
+            break;
+        case "red":
+            animations();
+            sound(red);
+            break;
+        case "yellow":
+            animations(yellow);
+           sound();
+            break;
+        case "blue":
+            animations(blue);
+            sound();
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+function pressKeyToStart() {
+    $("#level-title").text("level 1");
+    soundAndAnimation(randomChosenColour);
+    setTimeout(() => {
+        document.removeEventListener("keyup" , pressKeyToStart);
+    }, 100);
+}
+document.addEventListener("keyup" , pressKeyToStart);
---
+++ game2.js
@@ -0,0 +1,124 @@
+
+console.log("jai shree ram");
+let buttonColours = ["red", "blue", "green", "yellow"];
+let userClickedPattern = [];
+
+let level = 0;
+let start = false;
+
+let gamePattern = [];
+
+
+// document.addEventListener("keyup", callNextSequence);
+// function callNextSequence() {
+//     nextSequence();
+//     setTimeout(() => {
+//         document.removeEventListener("keyup", callNextSequence);
+//     }, 100);
+// }
+//better way of doing calling event listener for once 
+document.addEventListener("keyup",()=>{
+    if (!start) {
+        $(".btn").click(handler);
+function handler(event) {
+    let userChosenColour = event.target.id;//THIS NOT WORK HERE AND I FOUND AOUT THAT .TARGET METHOD WORKS WELL
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+}
+        nextSequence();
+        start = true;
+    }
+})
+
+
+
+
+        
+
+
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        // console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {//TO TRACK ALL THE PREVIOUS MATCHINGS BCZ ONLY CHECKING THE LAST ONE IS NOT TRUE
+            //WAHT IF THE PLAYER DID NOT GO THROUGH THE PATTERN 
+            setTimeout(() => {
+                nextSequence();
+                
+            }, 1000);
+        }
+    }
+    else {
+        // console.log("wrong") 
+        let wrong = "wrong"
+        playSound(wrong);
+        //CALLING AN EVENT LISTNER ONCE
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 100);
+        $("h1").text("Game Over , Refresh to  Restart");
+        // console.log(level);
+
+        //FOR CALLING A EVENT LISTNER JUST ONCE 
+        $(document).one('keyup', () => {
+            startOver();  // startOver will only be called Once
+          });
+
+        // let MaxLevel = level-1;
+        // localStorage.setItem("Maximum Level" , MaxLevel);
+        // $("h3").text("Maximum Score - " + localStorage.getItem("Maximum Level"));
+        
+
+       
+        
+       
+    };
+}
+
+function startOver() { //RESETTING ALL THE PARAMETRS 
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+    nextSequence();
+}
+
+
+
+
+function nextSequence() {
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("level " + level);
+   
+    // MaximumScore(level);
+
+    let randomNumber = Math.floor((Math.random() * 4));
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    animatePress(randomChosenColour);
+    playSound(randomChosenColour);
+}
+
+
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(() => {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+
+
+
+function playSound(name) {
+    let sound = new Audio('sounds/' + name + '.mp3');
+    sound.play();
+
+}
+
